ebe234b3f086615f6619d25e57112238a67860c9
irynagnatenko <Ulibka1023>
2022-04-04 13:48:04 +0200

    data models created

---
+++ src/main/java/se/b3/healthtech/blackbird/blbaggregator/enums/CompositionType.java
@@ -0,0 +1,7 @@
+package se.b3.healthtech.blackbird.blbaggregator.enums;
+
+public enum CompositionType {
+    COMPOSITION,
+    CONTAINER,
+    CONTAINER_OBJECT
+}
---
+++ src/main/java/se/b3/healthtech/blackbird/blbaggregator/enums/ContentType.java
@@ -0,0 +1,8 @@
+package se.b3.healthtech.blackbird.blbaggregator.enums;
+
+public enum ContentType {
+    H1,
+    H2,
+    H3,
+    CONTENT
+}
